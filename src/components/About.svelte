57a51e740e6abdfbbaf73e2426ed7ce5f486593f
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-04-17 03:32:20 -0500

    se agregó el componente about

---
+++ src/components/About.svelte
@@ -0,0 +1,18 @@
+<script>
+ let someVar = `Aprendiendo Svelte`;
+ let count = 0;
+ function handleClick() {
+    count += 1;
+ }
+</script>
+
+<style>
+ p {
+   color: var(--theme-color);
+ }
+</style>
+
+<div class="About">
+    <p>{someVar}</p>
+    <button on:click={handleClick}>Click {count == 0 ? '' : count}</button>
+</div>
