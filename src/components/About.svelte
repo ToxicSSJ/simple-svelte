29bcb777fa3c048292c1e1392ff3dae754ea3373
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-04-18 04:26:05 -0500

    simple darkmode

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,17 @@
 <script>
  let someVar = `Learning Svelte`;
  let count = 0;
+ let styles = { darkMode: false }
+
  function handleClick() {
     count += 1;
  }
+
+ function toggle() {
+   styles.darkMode = !styles.darkMode
+   window.document.body.classList.toggle("dark-mode")
+ }
+
 </script>
 
 <style>
@@ -13,6 +21,12 @@
 </style>
 
 <div class="About">
+
+  {#if !styles.darkMode}
     <p>{someVar}</p>
-    <button on:click={handleClick}>Click {count == 0 ? '' : count}</button>
+  {:else}
+    <p><span>Hello DarkMode</span></p>
+  {/if}
+  <button on:click={handleClick}>Click {count == 0 ? '' : count}</button>
+  <button on:click={toggle}>DarkMode</button>
 </div>
